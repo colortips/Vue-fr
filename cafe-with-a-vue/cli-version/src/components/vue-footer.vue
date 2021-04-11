6f83eeb20165e5b3ee0fa6386b2831e048afd2a4
colortips <h.schoen@intech-so.fr>
2021-04-11 20:40:27 +0200

    P2C3 done

---
+++ cafe-with-a-vue/cli-version/src/components/vue-footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="footer">
+    <p>{{ copyright }}</p>
+  </footer>
+</template>
+<script>
+export default {
+  name: "App",
+  data(){
+      return{
+          restaurantName: "La belle vue",
+      }
+  },
+  computed: {
+    copyright() {
+      const currentYear = new Date().getFullYear();
+      return `Copyright ${this.restaurantName} ${currentYear}`;
+    },
+  },
+};
+</script>
---
+++ cafe-with-a-vue/cli-version/src/views/Home.vue
@@ -0,0 +1,122 @@
+<template>
+  <div id="app" class="app">
+    <h1>{{ restaurantName }}</h1>
+    <p class="description">
+      Bienvenue dans notre café {{ restaurantName }}! Nous sommes réputés pour
+      notre pain et nos merveilleuses pâtisseries. Faites vous plaisir dès le
+      matin ou avec un goûter réconfortant. Mais attention, vous verrez qu'il
+      est difficile de s'arrêter.
+    </p>
+
+    <section class="menu">
+      <h2>Menu</h2>
+      <MenuItem
+				v-for="item in simpleMenu"
+				:addToShoppingCart="addToShoppingCart"
+				:name="item.name"
+				:image="item.image"
+				:quantity="item.quantity"
+				:inStock="item.inStock"
+				:key="item.name"
+			/>
+    </section>
+
+    <aside class="shopping-cart">
+      <h2>Panier d'achat : {{ shoppingCart }} articles</h2>
+    </aside>
+
+    
+    
+  </div>
+</template>
+
+<script>
+import MenuItem from '../components/MenuItem.vue'
+export default {
+  name: "App",
+  components: {
+    MenuItem
+  },
+  data() {
+    return {
+      address: "18 avenue du Beurre, Paris, France",
+      email: "hello@cafewithavue.bakery",
+      phone: "01 88 88 88 88",
+      restaurantName: "La belle vue",
+      shoppingCart: 0,
+      simpleMenu: [
+        {
+          name: "Croissant",
+          image: {
+            source: "/images/croissant.jpg",
+            alt: "Un croissant",
+          },
+          inStock: true,
+          quantity: 1,
+        },
+        {
+          name: "Baguette de pain",
+          image: {
+            source: "/images/french-baguette.jpeg",
+            alt: "Quatre baguettes de pain",
+          },
+          inStock: true,
+          quantity: 1,
+        },
+        {
+          name: "Éclair",
+          image: {
+            source: "/images/eclair.jpg",
+            alt: "Éclair au chocolat",
+          },
+          inStock: false,
+          quantity: 1,
+        },
+      ],
+    };
+  },
+  computed: {
+    copyright() {
+      const currentYear = new Date().getFullYear();
+      return `Copyright ${this.restaurantName} ${currentYear}`;
+    },
+  },
+  methods: {
+    addToShoppingCart(amount) {
+      this.shoppingCart += amount;
+    },
+  },
+};
+</script>
+<style lang="scss">
+.app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.description {
+  max-width: 960px;
+  font-size: 1.2rem;
+  margin: 0 auto;
+}
+
+.footer {
+  text-align: center;
+  font-style: italic;
+}
+
+.menu {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.shopping-cart {
+  position: absolute;
+  right: 30px;
+  top: 0;
+}
+</style>
